f4f44834373fac592c2702c30d653f29ed39b693
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-10-23 18:14:15 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+var candyString = 'foo';
+function addElementToBeginningOfArray(chocolateBars, candyString){
+  return[candyString,...chocolateBars]
+}
+function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
+  chocolateBars.unshift(candyString);
+  return chocolateBars;
+}
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return[...chocolateBars, candyString]
+  }
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
+  chocolateBars.push(candyString);
+}
+function accessElementInArray(chocolateBars, index) {
+  return (chocolateBars[2])
+}
