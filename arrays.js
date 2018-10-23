1490ca19cb9806fbc15cc9ab854a28a71c1537f6
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-10-23 18:36:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,6 +25,6 @@ function removeElementFromBeginningOfArray(chocolateBars) {
   return (chocolateBars);
 }
 function destructivelyRemoveElementFromEndOfArray(chocolateBars){
-  return (chocolateBars.pop());
+  chocolateBars.pop()
 }
 }
